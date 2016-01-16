72e07ce703a98f97c3dff8f9475c37f6561fb32f
Alex Kira <alex.kira@gmail.com>
2016-01-16 13:36:38 -0800

    Mix task - revert to previous version which manages application
    When exq_ui is included as a mix dependency, it would typically not be part
    of the OTP application list (as we don't need a separate server for it).
    This allows `mix.ui` to work by firing up the application in the mix task.

--- lib/mix/tasks/exq.ui.ex
+++ lib/mix/tasks/exq.ui.ex
@@ -4,7 +4,25 @@ defmodule Mix.Tasks.Exq.Ui do
   @shortdoc "Starts the Exq UI Server"
 
   def run(args) do
-    Mix.Task.run "run", ["--no-halt"] ++ args
+
+    web_namespace = Application.get_env(:exq_ui, :web_namespace, "")
+    web_port = Application.get_env(:exq_ui, :web_port, 4040)
+
+    # Start exq enqueuer supervisor
+    {:ok, _} = Exq.Enqueuer.Supervisor.start_link
+
+    api_name = Exq.Enqueuer.Supervisor.server_name(nil, :normal)
+    exq_opts = [name: api_name]
+
+    # Check connection
+    {:ok, _} = Exq.Api.queue_size(api_name)
+
+    IO.puts "Starting ExqUI on Port #{web_port}"
+
+    value = Plug.Adapters.Cowboy.http ExqUi.RouterPlug,
+      [namespace: web_namespace, exqopts: exq_opts], port: web_port
+
+    :timer.sleep(:infinity)
   end
 
 end
