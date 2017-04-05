e4c98cf025ab25916ed9d537a6a1718d188b65d6
Alex Kira <alex.kira@gmail.com>
2017-04-04 22:31:02 -0700

    Bump hex to 0.8.5
    [skipci]

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExqUi.Mixfile do
 
   def project do
     [ app: :exq_ui,
-      version: "0.8.4",
+      version: "0.8.5",
       elixir: "~> 1.3",
       elixirc_paths: ["lib", "web"],
       package: [
@@ -34,7 +34,7 @@ defmodule ExqUi.Mixfile do
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [
-      { :exq, "~> 0.8.3"},
+      { :exq, "~> 0.8.5"},
       { :plug, ">= 0.8.1 and < 2.0.0"},
       { :cowboy, "~> 1.0" },
       { :excoveralls, "~> 0.3", only: :test },
