f9eef0b84a4c4db6ba79c9dae3a2c2f7dcbe7542
Anantha Kumaran <ananthakumaran@gmail.com>
2021-08-21 15:38:45 +0530

    add batch delete for enqueued jobs

--- lib/exq_ui_web/live/queue_live/show.ex
+++ lib/exq_ui_web/live/queue_live/show.ex
@@ -6,6 +6,15 @@ defmodule ExqUIWeb.QueueLive.Show do
 
   @impl true
   def mount(%{"name" => name} = params, _session, socket) do
+    socket =
+      assign(socket, :columns, [
+        %{header: "Module", accessor: fn item -> item.job.class end},
+        %{header: "Arguments", accessor: fn item -> inspect(item.job.args) end}
+      ])
+      |> assign(:actions, [
+        %{name: "delete", label: "Delete"}
+      ])
+
     {:ok, assign(socket, jobs_details(name, params["page"] || "1"))}
   end
 
@@ -15,9 +24,19 @@ defmodule ExqUIWeb.QueueLive.Show do
   end
 
   @impl true
-  def handle_event("delete", %{"raw" => json, "queue" => name}, socket) do
-    :ok = Api.remove_enqueued_job(Exq.Api, name, json)
-    {:noreply, assign(socket, jobs_details(name, socket.assigns.page))}
+  def handle_event("action", %{"table" => %{"action" => "delete"} = params}, socket) do
+    name = socket.assigns.name
+
+    raw_jobs =
+      Map.delete(params, "action")
+      |> Map.values()
+
+    unless Enum.empty?(raw_jobs) do
+      :ok = Api.remove_enqueued_jobs(Exq.Api, name, raw_jobs)
+    end
+
+    socket = assign(socket, jobs_details(name, socket.assigns.current_page))
+    {:noreply, socket}
   end
 
   @impl true
@@ -31,19 +50,28 @@ defmodule ExqUIWeb.QueueLive.Show do
     {:noreply, socket}
   end
 
-  defp jobs_details(name, page) do
-    current_page =
+  defp jobs_details(name, page) when is_binary(page) do
+    page =
       case Integer.parse(page) do
         :error -> 1
         {page, _} -> page
       end
 
+    jobs_details(name, page)
+  end
+
+  defp jobs_details(name, page) do
     {:ok, total} = Api.queue_size(Exq.Api, name)
 
     {:ok, raw_jobs} =
-      Api.jobs(Exq.Api, name, raw: true, offset: @page_size * (current_page - 1), size: @page_size)
+      Api.jobs(Exq.Api, name, raw: true, offset: @page_size * (page - 1), size: @page_size)
 
-    jobs = Enum.map(raw_jobs, fn json -> %{job: Exq.Support.Job.decode(json), raw: json} end)
-    %{jobs: jobs, name: name, total: total, current_page: current_page, page_size: @page_size}
+    items =
+      Enum.map(raw_jobs, fn json ->
+        job = Exq.Support.Job.decode(json)
+        %{job: job, id: job.jid, raw: json}
+      end)
+
+    %{items: items, name: name, total: total, current_page: page, page_size: @page_size}
   end
 end
