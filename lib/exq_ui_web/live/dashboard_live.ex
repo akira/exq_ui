8666be0bf06ad8129055fe9cc18bc2e289976c11
Anantha Kumaran <ananthakumaran@gmail.com>
2021-08-22 13:00:46 +0530

    show stats in all the pages

--- lib/exq_ui_web/live/dashboard_live.ex
+++ lib/exq_ui_web/live/dashboard_live.ex
@@ -1,19 +1,8 @@
 defmodule ExqUIWeb.DashboardLive do
   use ExqUIWeb, :live_view
-  alias ExqUI.Queue
-
-  @tick_interval 10000
 
   @impl true
   def mount(_params, _session, socket) do
-    if connected?(socket), do: Process.send_after(self(), :tick, @tick_interval)
-
-    {:ok, assign(socket, stats: Queue.stats())}
-  end
-
-  @impl true
-  def handle_info(:tick, socket) do
-    Process.send_after(self(), :tick, @tick_interval)
-    {:noreply, assign(socket, :stats, Queue.stats())}
+    {:ok, socket}
   end
 end
