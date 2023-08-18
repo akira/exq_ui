24ced1e3fb1b0a792290d8f9ec3af376abf0553b
Anantha Kumaran <ananthakumaran@gmail.com>
2023-08-18 09:04:27 +0530

    drop support for phoenix live view 0.17

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 [
   import_deps: [:phoenix],
-  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"]
+  plugins: [Phoenix.LiveView.HTMLFormatter],
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs,heex}"]
 ]
