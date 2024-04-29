821401e47e6312143989fa3b2ea42258976ab76b
Herman Langner <hermandlangner@gmail.com>
2024-04-29 09:20:26 +0200

    index test more flexible

--- test/exq_ui_web/live/busy_live/index_test.exs
+++ test/exq_ui_web/live/busy_live/index_test.exs
@@ -5,9 +5,9 @@ defmodule ExqUIWeb.BusyLive.IndexTest do
     {:ok, view, _} = live(conn, "/busy")
     html = render(view)
 
-    assert html =~ ~r/Queues:.*hard, soft/
-    assert html =~ ~r/790fa550-08a4-42de-93e4-8c09c867befe/
-    assert html =~ ~r/0aec2714-9032-4574-ae45-a2037c874d9f/
+    assert html =~ ~r/Queues:.*hard, soft/s
+    assert html =~ ~r/790fa550-08a4-42de-93e4-8c09c867befe/s
+    assert html =~ ~r/0aec2714-9032-4574-ae45-a2037c874d9f/s
   end
 
   test "quiet", %{conn: conn} do
